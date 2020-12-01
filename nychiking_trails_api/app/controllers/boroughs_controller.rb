61c7aea94099987eb9f47d57a67c8de17766fbe8
JGamez17 <github email address>
2020-12-01 00:06:31 -0500

    first commit- created api backend & front end

---
+++ nychiking_trails_api/app/controllers/boroughs_controller.rb
@@ -0,0 +1,7 @@
+class BoroughsController < ApplicationController
+    def index
+         @boroughs = Borough.all
+         render json: @boroughs, except: [:created_at, :updated_at]
+    end
+    
+end
---
+++ nychiking_trails_frontend/src/index.js
@@ -0,0 +1,6 @@
+console.log("testing...");
+
+<script type="application/javascript" src="src/index.js" charset="UTF-8"></script>
+
+
+
