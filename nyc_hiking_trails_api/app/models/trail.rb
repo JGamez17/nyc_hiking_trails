8afc269c8ad933e1f42b19eea678fb8a53838f50
JGamez17 <github email address>
2020-12-16 19:54:32 -0500

    udpated backend to communicate with frontend and update likes properly

--- nyc_hiking_trails_api/app/models/trail.rb
+++ nyc_hiking_trails_api/app/models/trail.rb
@@ -1,4 +1,11 @@
 class Trail < ApplicationRecord
     belongs_to :borough
     has_many :comments
+
+    after_initialize :set_likes
+        def set_likes
+            self.likes ||= 0 
+        end
+
 end
+
--- nyc_hiking_trails_api/db/schema.rb
+++ nyc_hiking_trails_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_010241) do
+ActiveRecord::Schema.define(version: 2020_12_17_001529) do
 
   create_table "boroughs", force: :cascade do |t|
     t.string "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_12_010241) do
     t.string "length"
     t.string "difficulty"
     t.integer "borough_id"
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
